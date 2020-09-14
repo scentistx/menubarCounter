c66f5ce1930bae8efba38c8c9a8e8a6683c80511
scentistx <scentistx@gmail.com>
2020-09-14 09:59:46 +0300

    0.0.1

---
+++ menubarCounter/AppDelegate.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.swift
+//  menubarCounter
+//
+//  Created by Andrei Volkov on 11.09.2020.
+//  Copyright © 2020 Andrei Volkov. All rights reserved.
+//
+
+import Cocoa
+import SwiftUI
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Create the popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 200, height: 200)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+        
+        // Create the status item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "Icon")
+            button.action = #selector(togglePopover(_:))
+        }
+    }
+    
+    @objc func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
+    }
+    
+}
+
+struct AppDelegate_Previews: PreviewProvider {
+    static var previews: some View {
+        Text("menubarCounter by Andrei Volkov")
+    }
+}
---
+++ menubarCounter/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  menubarCounter
+//
+//  Created by Andrei Volkov on 11.09.2020.
+//  Copyright © 2020 Andrei Volkov. All rights reserved.
+//
+
+import SwiftUI
+
+struct BlueButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .foregroundColor(configuration.isPressed ? Color.blue : Color.white)
+            .background(configuration.isPressed ? Color.white : Color.blue)
+            .cornerRadius(60.0)
+            .padding(10.0)
+    }
+}
+
+struct ContentView: View {
+    @State var counter = 0
+    
+    var body: some View
+    {
+        VStack {
+            Text("Counter: \(self.counter)")
+                .multilineTextAlignment(.center)
+                .font(.system(size:16))
+                .font(.system(.largeTitle, design: .rounded))
+                .foregroundColor(.blue)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            Button(action: {
+                self.counter += 1
+            }) {
+                    
+                    Text("+")
+                        .font(.system(size: 40))
+                        .frame(maxWidth: 50, maxHeight: 40)
+                .padding(10.0)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 60.0)
+                        .stroke(lineWidth: 0.0)
+                        .shadow(color: .blue, radius: 10.0))
+            }
+            .buttonStyle(BlueButtonStyle())
+            
+            Button(action: {
+                self.counter -= 1
+            }) {
+                    Text("-")
+                        .font(.system(size:40))
+                        .frame(maxWidth: 50, maxHeight: 40)
+                .padding(10.0)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 60.0)
+                        .stroke(lineWidth: 0.0)
+                        .shadow(color: .blue, radius: 10.0))
+            }
+            .buttonStyle(BlueButtonStyle())
+            
+            
+            Button(action: {
+                self.counter = 0
+            }) {
+                    Text("⟲")
+                        .font(.system(size:25))
+                        .frame(maxWidth: 50, maxHeight: 40)
+                .padding(1.0)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 60.0)
+                        .stroke(lineWidth: 0.0)
+                        .shadow(color: .blue, radius: 10.0))
+            }
+            .buttonStyle(BlueButtonStyle())
+            
+            Spacer()
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
